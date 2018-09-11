5d6050d0a13b414b4c0b333ca2b673ea2957130f
El Omari <elomaridouae@gmail.com>
2018-09-11 19:37:06 +0200

    ucfirst - capitalize - camelCase Ok

---
+++ exercise-1/string.js
@@ -0,0 +1,25 @@
+function ucfirst(word){
+	if (word.length > 0) {
+    return word[0].toUpperCase() + word.substring(1);
+  } else {
+    return word;
+  }
+}
+
+function capitalize(word){
+	split = word.split(" ");
+
+	for ( var i = 0; i < split.length; i++ )
+    {
+        var j = split[i].charAt(0).toUpperCase();
+        split[i] = j + split[i].substr(1);
+    }
+    return split.join(" ");
+}
+
+function camelCase(word){
+	mots = capitalize(word);
+	split = mots.split(" ");
+	return split.join("");
+}
+console.log(camelCase("hello world"));
